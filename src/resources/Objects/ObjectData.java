34deceb4956929d41c639ca3695b3280d0935a69
saugat694260 <khadkasaugat321@gmail.com>
2025-06-06 16:26:03 +0545

    added folders

---
+++ src/resources/Objects/ObjectData.java
@@ -0,0 +1,10 @@
+package resources.Objects;
+
+public class ObjectData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/resources/Person/CharactersData.java
@@ -0,0 +1,10 @@
+package resources.Person;
+
+public class CharactersData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
