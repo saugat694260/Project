bc612bdfdbfdc9ac70062104cc847d84fb6f29ff
questofjoy <questofjoyy@gmail.com>
2025-06-06 22:59:23 +0900

    added a bit of login and reg
    can now make a user

---
+++ src/LoginAndRegister/LoginAndRegister.java
@@ -0,0 +1,82 @@
+package LoginAndRegister;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class LoginAndRegister {
+  // to avoid hardcoding file paths when other users use this program
+  private static final String DIR_CURRENT_PATH = Paths.get("").toAbsolutePath().toString();
+  // file.seperator adds the file seperator symbol accoiding to the machine that
+  // user is curently on
+  // eg: "/" for unix and "\\" for windows
+  // toAbsolutePath is required caus we aint hardcoding this folder anywhere on
+  // other places as of now
+  private static final String DIR_USER_DATA = Paths.get("").toAbsolutePath().toString() + File.separator + "userData";
+
+  public static void userDirInitialization() {
+    File userData = new File(DIR_USER_DATA);
+
+    // .mkdirs makes directories
+    // checking if the file was made, if not it will be created
+    // if it exists nothing will be done
+    if (userData.mkdir()) {
+      System.out.println("Folder userData was made!");
+    } else {
+      System.out.println("Folder userData already exists!");
+    }
+  }
+
+  public static void makeUserProfile(String userName, String userPassword) {
+    String userFilePath = DIR_USER_DATA + File.separator + userName + ".txt";
+    File userProfile = new File(userFilePath);
+
+    boolean userExists = false;
+
+    try {
+      // checking if the user already exists
+      if (!userProfile.createNewFile()) {
+        System.out.println("User with that name already exists!");
+        userExists = true;
+      }
+      System.out.println("New user created");
+      // catching io IOException to the variable e
+      // gotta use try cauz it will throw IOException if anything goes wrong while
+      // creating a new file
+      // pretty sure code wont run if i dont catch that IOException
+      // but i have not tested it
+      // so we are catching that IOException
+    } catch (IOException e) {
+      // prints errors making it easier to debug
+      e.printStackTrace();
+    }
+
+    // executes if user did not exist prior to creating new user from the above code
+    if (!userExists) {
+      // creates a new buffered BufferedWriter
+      // a simple version of it would be
+      //
+      // FileWriter fw = new FileWriter(userFilePath);
+      // BufferedWriter bfw = new BufferedWriter(fw);
+      //
+      // difference between filewriter and BufferedWriter is that
+      // buffered writer writes a whole string at once
+      // while filewriter writes every character once at a time
+      // we are using buffered writer because there might be other user stats that
+      // will be added in future
+      try (BufferedWriter bfw = new BufferedWriter(new FileWriter(userFilePath))) {
+        bfw.append(userName);
+        bfw.newLine();
+        bfw.append(userPassword);
+        bfw.newLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+  }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import LoginAndRegister.*;
+
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-    System.out.println("hellow world"); // prints hellow world
-    System.out.println("hellow world"); // prints hellow world
+    LoginAndRegister.userDirInitialization();
+    LoginAndRegister.makeUserProfile("lmao", "lmao");
+    // close scanner at the very end of the program
+    scan.close();
   }
 }
