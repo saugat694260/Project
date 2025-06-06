bdefa9dfc07e0bf4343856e6bf377aed2c5bd1d8
questofjoy <questofjoyy@gmail.com>
2025-06-06 20:11:37 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		System.out.println("hellow world"); //prints hellow world
+  public static void main(String[] args) {
 
-	}
+    System.out.println("hellow world"); // prints hellow world
+    System.out.println("hellow world"); // prints hellow world
+
+  }
 
 }
