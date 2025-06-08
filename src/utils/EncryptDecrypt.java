ae1a2181467a702c0bf0516c389712c37c702ac9
saugat694260 <135124762+saugat694260@users.noreply.github.com>
2025-06-08 16:03:03 +0545

    Update EncryptDecrypt.java

--- src/utils/EncryptDecrypt.java
+++ src/utils/EncryptDecrypt.java
@@ -2,10 +2,10 @@ package utils;
 
 public class EncryptDecrypt {
 
-    String a ="!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_` abcdefghijklmnopqrstuvwxyz{|}~";
+    private String a ="!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_` abcdefghijklmnopqrstuvwxyz{|}~";
     private char[] list = a.toCharArray();
     
-    String b= "_DkZ8QzmVAOhHGSR|UY7\"i@bj`L,cud<3?&4Nsw:TXKx}Pv/W;!tf5^)(*I$rF6 q9lE\\0~%2aJ=y'B1onM{#>]ep[gC+-.";
+    private String b= "_DkZ8QzmVAOhHGSR|UY7\"i@bj`L,cud<3?&4Nsw:TXKx}Pv/W;!tf5^)(*I$rF6 q9lE\\0~%2aJ=y'B1onM{#>]ep[gC+-.";
     private char[] shuffeledList = b.toCharArray();
     public EncryptDecrypt() {
      //
