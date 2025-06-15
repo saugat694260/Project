f18172cd4b991544c25ced85811a2f91a121d538
saugat694260 <khadkasaugat321@gmail.com>
2025-06-15 23:59:32 +0545

    just a sample for extracting data, efficient wat still left

--- src/main/java/utils/readJson.java
+++ src/main/java/utils/readJson.java
@@ -1,25 +1,31 @@
 package utils;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.Reader;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import utils.getData;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class readJson {
 
     public static void readAndPrintCharacters() {
-    	
         Gson gson = new GsonBuilder().create();
 
-        try (Reader reader = Files.newBufferedReader(Paths.get("src/main/java/gameData/characterData.json"))) {
+        // Load resource from classpath
+        InputStream is = readJson.class.getResourceAsStream("/gameData/characterData.json");
 
+        if (is == null) {
+            System.err.println("ERROR: Resource /gameData/characterData.json not found!");
+            return;
+        }
+
+        try (Reader reader = new InputStreamReader(is)) {
             getData.GameCharacterList characterList = gson.fromJson(reader, getData.GameCharacterList.class);
 
             for (getData.GameCharacter character : characterList.gameCharacters) {
                 System.out.println("Name: " + character.name +
-                        ", Class: " + character.clazz+
+                        ", Class: " + character.clazz +
                         ", Level: " + character.level);
             }
 
